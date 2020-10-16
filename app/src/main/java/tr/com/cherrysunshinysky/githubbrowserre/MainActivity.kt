7f65d2951cef3be89bbc68c6778f558e5a22d206
skywind83 <emir.ozen@outlook.com>
2020-10-16 15:07:57 +0300

    Initial commit

---
+++ app/src/main/java/tr/com/cherrysunshinysky/githubbrowserre/MainActivity.kt
@@ -0,0 +1,11 @@
+package tr.com.cherrysunshinysky.githubbrowserre
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
